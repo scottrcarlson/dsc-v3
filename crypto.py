6cecbb330630329ac23f86c7387a68df94dac34e
Scott R Carlson <scott.r.carlson@gmail.com>
2018-04-06 22:19:39 -0400

    Removed static IV

--- crypto.py
+++ crypto.py
@@ -9,8 +9,9 @@ from config import Config
 import logging
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from base64 import b64encode
+from os import urandom
 
-IV = 'A' * 16 # TODO: fixme
 BS = 16
 
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
@@ -26,7 +27,7 @@ class Crypto(object):
 		#self.log.setLevel(logging.DEBUG)
 
 	def encrypt(self, key, pt):
-		self.log.debug("was asked to encrypt this plaintext (len: %d):  %s" % (len(pt), binascii.hexlify(pt)))
+		#self.log.debug("was asked to encrypt this plaintext (len: %d):  %s" % (len(pt), binascii.hexlify(pt)))
 
 		#self.log.debug("key(%d): %s" %(len(key), key))
 		key = pad(key)
@@ -34,25 +35,32 @@ class Crypto(object):
 		#self.log.debug("iv(%d): %s" % (len(IV), binascii.hexlify(IV)))
 		pt = pad(pt)
 		#self.log.debug("padded_pt(%d): %s" % (len(pt), binascii.hexlify(pt)))
-		cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
+		iv = self.generateIV()
+		cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
 		encryptor = cipher.encryptor()
 		ct = encryptor.update(pt) + encryptor.finalize()
 		#self.log.debug("ct(%d): %s" % (len(ct), binascii.hexlify(ct)))
-		return ct
+		return iv + ct
 
 	def decrypt(self, key, ct):
-		self.log.debug("was asked to decrypt this ciphertext(len: %d): %s " % (len(ct), binascii.hexlify(ct)))
+		#self.log.debug("was asked to decrypt this ciphertext(len: %d): %s " % (len(ct), binascii.hexlify(ct)))
 		#self.log.debug("key(%d): %s" %(len(key), binascii.hexlify(key)))	
 		key = pad(key)
 		#self.log.debug("padded_key(%d): %s" %(len(key), binascii.hexlify(key)))
 		#self.log.debug("iv(%d): %s" % (len(IV), binascii.hexlify(IV)))
-		cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=default_backend())
+
+		iv = ct[:16]
+		ct = ct[16:]
+		cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
 		decryptor = cipher.decryptor()
 		pt = decryptor.update(ct) + decryptor.finalize()
 		pt = unpad(pt)
 		#self.log.debug("unpadded_pt(%d): (%s)" % (len(pt), binascii.hexlify(pt)))
 		return pt
 
+	def generateIV(self):
+		random_bytes = urandom(16)
+		return random_bytes
 
 if __name__ == "__main__":
 	#c = Crypto()
